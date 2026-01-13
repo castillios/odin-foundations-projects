91b182613a0504790f1571edd2e0484ba90b5d03
castillios <maleahcast@gmail.com>
2026-01-13 03:07:47 -0800

    Edit rps script.js

--- rps/script.js
+++ rps/script.js
@@ -1,6 +1,3 @@
-let humanScore = 0;
-let computerScore = 0;
-
 function getComputerChoice() {
     let randInt = Math.floor(Math.random() * 3);
 
@@ -15,9 +12,68 @@ function getComputerChoice() {
 }
 
 function getHumanChoice() {
-    let humanChoice = prompt("Rock, paper, scissors?");
+    let humanChoice = prompt("rock, paper, scissors?");
     return humanChoice;
 }
 
-// console.log(getComputerChoice())
-console.log(getHumanChoice())
+function playRound(humanChoice, computerChoice, humanScore, computerScore) {
+    let humanWon = false;
+    let isTied = false;
+    if (humanChoice === "rock") {
+        if (computerChoice === "scissors") {
+            humanScore += 1;
+            humanWon = true;
+        } else if (computerChoice === "paper") {
+            computerScore += 1;
+        } else {
+            isTied = true;
+        }
+    } else if (humanChoice === "paper") {
+        if (computerChoice === "rock") {
+            humanScore += 1;
+            humanWon = true;
+        } else if (computerChoice === "scissors") {
+            computerScore += 1;
+        } else {
+            isTied = true;
+        }
+    } else if (humanChoice === "scissors") {
+        if (computerChoice === "paper") {
+            humanScore += 1;
+            humanWon = true;
+        } else if (computerChoice === "rock") {
+            computerScore += 1;
+        } else {
+            isTied = true;
+        }
+    }
+    console.log(`You played: ${humanChoice}. Computer played: ${computerChoice}.`)
+    if (isTied) {
+        console.log(`${humanChoice.slice(0,1).toUpperCase()}${humanChoice.slice(1)} ties with ${computerChoice}.`)
+    }
+    else if (humanWon) {
+        console.log(`${humanChoice.slice(0,1).toUpperCase()}${humanChoice.slice(1)} beats ${computerChoice}! Player +1`)
+    } else {
+        console.log(`${humanChoice.slice(0,1).toUpperCase()}${humanChoice.slice(1)} loses to ${computerChoice}! Computer +1`)
+    }
+    console.log(`Player: ${humanScore} Computer: ${computerScore}`)
+    return [humanScore, computerScore];
+}
+
+function playGame(rounds) {
+    let scores = {
+        humanScore: 0,
+        computerScore: 0
+    };
+
+    for (let i = 0; i < rounds; i++) {
+        console.log(`Round ${i + 1}`)
+        updatedScores = playRound(getComputerChoice(), getComputerChoice(), scores.humanScore, scores.computerScore);
+        scores.humanScore = updatedScores[0];
+        scores.computerScore = updatedScores[1];
+    }
+}
+
+
+
+playGame(10);
